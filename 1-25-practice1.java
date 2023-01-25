9b86c573f24c33c8eebbd5f48e70e89299499636
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2023-01-25 19:52:15 +0900

    23

---
+++ 1-25-practice1.java
@@ -0,0 +1,49 @@
+package Practice;
+
+public class Practice3 {
+	
+//	int a = 10; // 클래스 내부에서 사용 가능한 변수
+//	
+//	static int b = 20; // practice3 클래스 내부에서 '전역'으로 사용가능 (전역변수)
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello JAVA Project");
+			
+//			boolean run = true; // boolean 자료형의 변수 'run'을 선언 후 True로 초기화
+//			
+//			int i = 10; // 4byte 크기 정수 자료형
+			
+//			int j = 5; // 메인 메소드 내에서 사용가능한 변수(지역변수)
+			
+//			System.out.println(a); a라는 변수는 메인메소드 내에서 선언된적 없어 에러.
+//			System.out.println("지역변수: "+j);
+//			System.out.println("전역변수: "+b);
+//			
+//			double PI = 3.141592;
+//			
+//			int pi = (int)PI; // double->integer 형 변환
+//			System.out.println("형 변환 이전:"+PI);
+//			System.out.println("형 변환 이후:"+pi);
+			
+//			int a = 10;
+//			int b = 4;
+//			System.out.println("나눗셈 연산 후 형변환: "+(double)a/b);
+			
+//			int a = 10;
+//			int b = ++a;
+//			
+//			System.out.println("전위연산 결과: "+b);
+//			// b는 10에서 전위연산이 되어 11로 출력
+//			
+//			int c = 10;
+//			int d = c++;
+//			
+//			System.out.println("후위연산 결과: "+d); // d는 후위연산이라 아직 연산 전이므로 10으로 나온다
+//			System.out.println("이제 후위연산 적용됨: "+c);
+			
+//			System.out.println("10진수 2를 2진수로 변환: "+ Integer.toBinaryString(2));
+//			System.out.println("integer 정수 자료형의 최대값: "+Integer.MAX_VALUE);
+	}
+
+}
