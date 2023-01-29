c67b9de647dc74695a956f978d1bb1aad84ed685
JOE YOUNG JAE <50972003+mnyu123@users.noreply.github.com>
2023-01-29 18:12:54 +0900

    30

---
+++ 1-29-example1.java
@@ -0,0 +1,43 @@
+public class 1-29-example1 {
+    package Prac2;
+
+public class Chapter5_practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//--------------연습문제 1번--------------------
+//		int sum = 0;
+//
+//		for (int i = 1; i < 101; i++) {
+//			if (i % 5 == 0) {
+//				sum += i;
+//			}
+//		}
+//
+//		System.out.println("5의 배수의 합계는 " + sum);
+//-------------------------------------------
+		
+//--------------연습문제 2번--------------------
+//		int evensum = 0;
+//		int oddsum = 0;
+//		
+//		for(int i=1;i<=100;i++) {
+//			if(i%2 == 1) {
+//				evensum += i;
+//			}
+//			else if(i%2 == 0) {
+//				oddsum += i;
+//			}
+//		}
+//		
+//		System.out.println("짝수의 합계는 :"+oddsum);
+//		System.out.println("홀수의 합계는 :"+evensum);
+//-------------------------------------------
+		
+//--------------연습문제 3번--------------------
+		
+	}
+
+}
+
+}
