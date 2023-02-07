9b5c477c6456dbf1bb3befa7e0081a1d5113a22a
mnyu123 <whdudwo5582@gmail.com>
2023-02-07 17:49:01 +0900

    41

--- 2-6-SecondTime/2-6-Game1.java
+++
@@ -1,8 +0,0 @@
-package test4;
-
-public class Game1 {
-	void Display(GraphicCard g) { // 부모클래스 타입을 매개변수로 정의
-		g.GraphicProcess(); // '그래픽 프로세스' 라는 이름으로 실행하게 되면 
-							// --> 자식 클래스의 내용이 실행이 가능함. => 다형성
-	}
-}
