3bde4793f479e05e34beec0d90850a766a0c64ca
mnyu123 <whdudwo5582@gmail.com>
2023-02-16 15:53:55 +0900

    53

--- 2-15-SecondTime/InExam2.java
+++
@@ -1,9 +0,0 @@
-package Test7;
-
-public class InExam2 implements Outer2.exam {
-	@Override
-	public void method() {
-		// TODO Auto-generated method stub
-		System.out.println("InExam2클래스 의 exam 메소드 실행함.");
-	}
-}
